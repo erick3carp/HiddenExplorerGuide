27f63100e3cad7d787e72efdeca4d2f1e02a3065
Erick Carpio <erick3carp@gmail.com>
2026-06-22 17:41:59 -0400

    Remove .next build files from Git

--- .next/static/chunks/app/about/page-a140f05feb187f69.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,883],{9531:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=9531)}),_N_E=n.O()}]);
